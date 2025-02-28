e20436e921fcbac1ad3b0ed9fc68cc7c53f8374a
sivaramanand <notifysiva@gmail.com>
2025-02-28 15:38:23 +0530

    changes 1

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import { useTheme } from "./ThemeContext";
-import "./individual.css"
+import "./individual.css";
 
 function ProjectCard({ src, h3, p, live_link, code_link }) {
   const { theme } = useTheme();
@@ -8,27 +8,32 @@ function ProjectCard({ src, h3, p, live_link, code_link }) {
     <div className="individual">
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
-      <div className="pdiv" style={{maxWidth:"400px"}}>
+      <div className="pdiv" style={{ maxWidth: "400px" }}>
         <p>{p}</p>
       </div>
       <div className="linksection">
         <div>
-          <a
-            style={{ color: theme === "dark" ? "white" : "black" }}
-            href={live_link}
-            target="_blank"
-          >
-            Live link
-          </a>
+          <p className="ap">
+            {" "}
+            <a
+              style={{ color: theme === "dark" ? "white" : "black" }}
+              href={live_link}
+              target="_blank"
+            >
+              Live link
+            </a>
+          </p>
         </div>
         <div>
-          <a
-            style={{ color: theme === "dark" ? "white" : "black" }}
-            href={code_link}
-            target="_blank"
-          >
-            Code link
-          </a>
+          <p className="ap">
+            <a
+              style={{ color: theme === "dark" ? "white" : "black" }}
+              href={code_link}
+              target="_blank"
+            >
+              Code link
+            </a>
+          </p>
         </div>
       </div>
     </div>
