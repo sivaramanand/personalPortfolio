1ed4e53fe9b3a4220b0c4c8a7451ac295595e02c
sivaramanand <notifysiva@gmail.com>
2025-02-28 13:01:22 +0530

    font colour cahnge

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTheme } from "./ThemeContext";
 
 function ProjectCard({ src, h3, p, live_link, code_link }) {
+  const { theme } = useTheme();
+
   return (
     <>
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
       <p>{p}</p>
-      <a href={live_link} target="_blank">
+      <a
+        style={{ color: theme === "dark" ? "white" : "black" }}
+        href={live_link}
+        target="_blank"
+      >
         Live link
       </a>
-      <a href={code_link} target="_blank">
+      <a
+        style={{ color: theme === "dark" ? "white" : "black" }}
+        href={code_link}
+        target="_blank"
+      >
         Code link
       </a>
     </>
